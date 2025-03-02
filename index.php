614a06c24b0f43238870351607e32dae0873a34e
msagatijr <msagatics@gmail.com>
2025-03-02 08:56:47 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,222 @@
+<?php
+  include ("header.php");
+?>
+
+
+      <div class="container">
+        <div class="row justify-content-center mt-3">
+          <div class="col-lg-8 col-sm-6 col-xs-6">
+            <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-interval="6000"
+              data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                  aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                  aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                  aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="./house/bailey-anselme-Bkp3gLygyeA-unsplash.jpg" class="banner-img d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="./house/digital-marketing-agency-ntwrk-g39p1kDjvSY-unsplash.jpg"
+                    class="banner-img d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="./house/harper-van-mourik-0yfWDwHOB0g-unsplash.jpg" class="banner-img d-block w-100"
+                    alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-sm-6 col-xs-6 align-content-center">
+            <h1 class="text-center">Welcome to your next <span class="go-span">rental</span> house.</h1>
+            <p class="mt-4">Bootstrap IntelliSense is a Visual Studio Code extension that enhances the development
+              experience with
+              Bootstrap. It offers CSS class autocompletion, reducing errors and saving time for developers. This tool
+              simplifies working with Bootstrap, making the development process more efficient and effective.</p>
+            <div class="row text-center">
+              <div class="col-12">
+                <button type="button" class="btn btn-success mt-3 px-4 py-2 me-2">Get Started</button>
+                <button type="button" class="btn btn-outline-primary mt-3 px-4 py-2">Call Us!</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container mx-auto">
+        <div class="row my-5 d-flex flex-column align-content-center text-center bg-body-tertiary py-4 rounded-2">
+          <div class="col-8">
+            <div class="btn-group mb-3" role="group" aria-label="Basic radio toggle button group">
+              <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+              <label class="btn btn-outline-primary px-4" for="btnradio1">Buy</label>
+
+              <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+              <label class="btn btn-outline-primary px-4" for="btnradio2">Rent</label>
+
+              <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+              <label class="btn btn-outline-primary px-4" for="btnradio3">Sell</label>
+            </div>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2 rounded-2" type="search"
+                placeholder="Enter an address, neighborhood, city, county or ZIP code" aria-label="Search">
+              <button class="btn btn-outline-success px-5" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row my-5">
+          <h2 class="text-center mb-0 font-weight-bold">Explore homes on neighborhood.</h2>
+          <p class="text-center mb-5">Take a deep dive and browse homes for sale, original neighborhood photos, resident
+            reviews and <br> local insights to find what is right for you.</p>
+          <div class="col-12 d-flex justify-content-around">
+            <div class="card" style="width: 18rem;">
+              <img src="./house/bailey-anselme-Bkp3gLygyeA-unsplash.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="./house/digital-marketing-agency-ntwrk-g39p1kDjvSY-unsplash.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="./house/dillon-kydd-2keCPb73aQY-unsplash.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="./house/harper-van-mourik-0yfWDwHOB0g-unsplash.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row my-5">
+          <div class="col-12 d-flex justify-content-around">
+            <div class="card" style="width: 18rem;">
+              <img src="./house/john-fornander-Id7u0EkTjBE-unsplash.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="./house/ronnie-george-9gGvNWBeOq4-unsplash.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="./house/zac-gudakov-wwqZ8CM21gg-unsplash.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="./house/todd-kent-178j8tJrNlc-unsplash.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                  card's content.</p>
+              </div>
+            </div>
+          </div>
+          <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-center mt-4">
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div><!--Container Fluid-->
+  </div><!--End-Container-->
+  <div class="container-fluid">
+    <div class="row my-5">
+      <div class="col-12 dr-banner text-center text-white py-5">
+        <h1>LET YOUR DREAMS COME TRUE</h1>
+        <h5>BUILDING HAPPY HOMES, CREATING HEALTHY LIVING SPACES.</h5>
+        <p class="w-50 mx-auto py-3">Navbar navigation links build on our .nav options with their own modifier class
+          and require the use of toggler classes for proper responsive styling. Navigation in navbars will also grow
+          to occupy as much horizontal space as possible to keep your navbar contents securely aligned.</p>
+        <button type="button" class="btn btn-outline-light">SEE OUR STORY</button>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row rounded-2 my-5">
+      <h1 class="text-center my-5">See how Real<span class="go-span">Estate</span> can help</h1>
+      <div class="col-12 d-flex justify-content-evenly text-center">
+        <div class="card border-0 bg-body-tertiary" style="width: 21rem;">
+          <img src="./icons/BuyAHome.svg"
+            class="card-img-top img-thumbnail img-fluid border-0 h-50 w-50 mx-auto bg-body-tertiary" alt="...">
+          <div class="card-body p-0">
+            <h5 class="card-title">Buy a home</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-info">Find a home</a>
+          </div>
+        </div>
+        <div class="card border-0 bg-body-tertiary" style="width: 21rem;">
+          <img src="./icons/RentAHome.svg"
+            class="card-img-top img-thumbnail img-fluid border-0 h-50 w-50 mx-auto bg-body-tertiary" alt="...">
+          <div class="card-body p-0">
+            <h5 class="card-title">Find a rental</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-primary">Find a rental</a>
+          </div>
+        </div>
+        <div class="card border-0 bg-body-tertiary" style="width: 21rem;">
+          <img src="./icons/Neighborhoods.svg"
+            class="card-img-top img-thumbnail img-fluid border-0 h-50 w-50 mx-auto bg-body-tertiary" alt="...">
+          <div class="card-body p-0">
+            <h5 class="card-title">Explore neighborhood</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn btn-dark text-white">Learn more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php
+  include ("footer.php");
+  ?>
